8a2c6a681cafa907704daf933b80394dc275809f
mihaiconstantin <mihai@mihaiconstantin.com>
2022-04-13 09:32:17 +0200

    Add `uninstall` action to `helpmessage`

--- battery.sh
+++ battery.sh
@@ -32,6 +32,9 @@ Usage:
   battery update
     run the installation command again to pull latest version
 
+  battery uninstall
+    enable charging and remove the `smc` tool and the `battery` script
+
 "
 
 # Visudo instructions
