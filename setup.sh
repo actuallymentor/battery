dc775e7577e89c6e68fbd727381e5ea727505477
Mentor <mentor@palokaj.co>
2022-09-06 15:42:46 +0200

    ✨ Pretties setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # User welcome message
-echo -e '\n👋 Welcome, this is the setup script for the battery CLI tool.'
-echo -e "Note: this script will ask for your password once or multiple times. This is needed install battery to your system."
+echo -e "\n####################################################################"
+echo '👋 Welcome, this is the setup script for the battery CLI tool.'
+echo -e "Note: this script will ask for your password once or multiple times."
+echo -e "####################################################################\n\n"
 
 # Ask for sudo once, in most systems this will cache the permissions for a bit
-sudo echo
+sudo echo "Superuser permissions acquired."
 
 # Get smc source and build it
 tempfolder=~/.battery-tmp
@@ -13,21 +15,21 @@ binfolder=/usr/local/bin
 mkdir -p $tempfolder
 
 smcfolder="$tempfolder/smc"
-echo -e "\nCloning fan control version of smc"
+echo "Cloning fan control version of smc"
 git clone --depth 1 https://github.com/hholtmann/smcFanControl.git $smcfolder &> /dev/null
 cd $smcfolder/smc-command
-echo -e "\nMaking smc from source"
+echo "Making smc from source"
 make &> /dev/null
 
 # Move built file to bin folder
-echo -e "\nMove smc to executable folder"
+echo "Move smc to executable folder"
 sudo mkdir -p $binfolder
 sudo mv $smcfolder/smc-command/smc $binfolder
 sudo chmod u+x $binfolder/smc
 
 # Write battery function as executable
 bateryfolder="$tempfolder/battery"
-echo -e "\nCloning battery repository"
+echo "Cloning battery repository"
 git clone --depth 1 https://github.com/actuallymentor/battery.git $bateryfolder &> /dev/null
 echo "Writing script to $binfolder/battery"
 sudo cp $bateryfolder/battery.sh $binfolder/battery
@@ -36,8 +38,8 @@ sudo chmod u+x $binfolder/battery
 
 # Remove tempfiles
 cd ../..
-echo -e "\nRemoving temp folder $tempfolder"
+echo "Removing temp folder $tempfolder"
 rm -rf $tempfolder
-echo -e "\nSmc binary built"
+echo "Smc binary built"
 
 echo -e "\n🎉 Battery tool installed. Type \"battery\" for instructions."
