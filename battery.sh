abadee8c144fbae6dc2aaf9033a3a212d298516d
Mentor <mentor@palokaj.co>
2023-04-21 10:45:15 +0200

    🔼 bump visudo folder fix

--- battery.sh
+++ battery.sh
@@ -4,7 +4,7 @@
 ## Update management
 ## variables are used by this binary as well at the update script
 ## ###############
-BATTERY_CLI_VERSION="v1.0.11"
+BATTERY_CLI_VERSION="v1.0.12"
 
 # Path fixes for unexpected environments
 PATH=/bin:/usr/bin:/usr/local/bin:/usr/sbin:/opt/homebrew
