bfcd5b155c5b65631eea6ef1d60a1aafde9e4d6d
Mentor <mentor@palokaj.co>
2023-08-24 16:22:38 +0200

    🔥 single-line logging of missing visudo perms

--- battery.sh
+++ battery.sh
@@ -4,7 +4,7 @@
 ## Update management
 ## variables are used by this binary as well at the update script
 ## ###############
-BATTERY_CLI_VERSION="v1.1.4"
+BATTERY_CLI_VERSION="v1.1.5"
 
 # Path fixes for unexpected environments
 PATH=/bin:/usr/bin:/usr/local/bin:/usr/sbin:/opt/homebrew
@@ -130,8 +130,7 @@ function change_magsafe_led_color() {
 	if sudo -n smc -k ACLC -r &>/dev/null; then
 		log "💡 Setting magsafe color to $color"
 	else
-		log "🚨 Your version of battery is using an old visudo file"
-		log "please run 'battery visudo' to fix this, until you do battery cannot change magsafe led colors"
+		log "🚨 Your version of battery is using an old visudo file, please run 'battery visudo' to fix this, until you do battery cannot change magsafe led colors"
 		return
 	fi
 
