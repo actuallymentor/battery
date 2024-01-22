7295f0fc72250decd3c1ad6571561e1df8b9a892
tangb4c <tangb4c@gmail.com>
2024-01-22 23:39:12 +0800

    update readme and version

--- battery.sh
+++ battery.sh
@@ -4,7 +4,7 @@
 ## Update management
 ## variables are used by this binary as well at the update script
 ## ###############
-BATTERY_CLI_VERSION="v1.1.6"
+BATTERY_CLI_VERSION="v1.2.0"
 
 # Path fixes for unexpected environments
 PATH=/bin:/usr/bin:/usr/local/bin:/usr/sbin:/opt/homebrew/bin:/opt/homebrew/sbin:/opt/homebrew
